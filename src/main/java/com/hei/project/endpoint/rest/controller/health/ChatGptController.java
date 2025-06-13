5ac533332dcaf522c2c495dad33fe619dfcd4537
tsanta22kyle <hei.tsanta.2@gmail.com>
2025-06-13 09:57:53 +0300

    fix:re name H to h

--- src/main/java/com/hei/project/endpoint/rest/controller/health/ChatGptController.java
+++ src/main/java/com/hei/project/endpoint/rest/controller/health/ChatGptController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/Hazavao")@AllArgsConstructor
+@RequestMapping("/hazavao")@AllArgsConstructor
 public class ChatGptController {
 
     private final ChatGptService chatGptService;
